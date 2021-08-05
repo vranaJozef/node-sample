5bb327e995dc749f41cf15239f734052ba72d239
jozef.vrana <jozef.vrana@inzata.com>
2021-08-06 00:17:55 +0200

    Init

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require(‘express’);
+const app = express();
+app.get(‘/’, function(req,res){
+  res.send(“Hi There”);
+});
+const port = 3000;
+app.listen(port, function () {
+  console.log(‘Listening on port 3000!’)
+})
