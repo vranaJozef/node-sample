60300c4204d19b5841f2460d8d47c352b59be5bf
vranaJozef <48406402+vranaJozef@users.noreply.github.com>
2021-08-06 01:48:53 +0200

    Delete test.js

--- test/test.js
+++
@@ -1,9 +0,0 @@
-const request = require("supertest");
-const app = require("../index");
-
-describe("GET /", () => {
-    it("responds with Hello, This Is The Default!", (done) => {
-        request(app).get("/").expect("Hello, This Is The Default!", done);
-    })
-});
-
