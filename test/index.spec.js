871775cdd5b537ca540ce1c69002fdd8614ae6ac
vranaJozef <48406402+vranaJozef@users.noreply.github.com>
2021-08-06 02:41:09 +0200

    Update index.spec.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,6 @@
 describe("Testing our nodeJS app", () => {
     it("Testing using Github Actions", () => {
+        var assert = require('assert')
         assert.equal([1, 2, 3].indexOf(2), 1);
     });
 });
